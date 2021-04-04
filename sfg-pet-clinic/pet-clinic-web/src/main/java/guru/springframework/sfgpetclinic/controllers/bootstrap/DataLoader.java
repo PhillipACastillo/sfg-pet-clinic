f4e5ce31dcef40701fb908960640ca0d9024726c
Phillip Castillo <PhillipACastillo@gmail.com>
2021-04-03 19:16:54 -0700

    Add pets and contact info to bootstrapped owners

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/bootstrap/DataLoader.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.controllers.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
@@ -8,9 +9,12 @@ import guru.springframework.sfgpetclinic.services.VetService;
 import guru.springframework.sfgpetclinic.services.map.OwnerServiceMap;
 import guru.springframework.sfgpetclinic.services.map.VetServiceMap;
 import jdk.jfr.Percentage;
+import org.apache.tomcat.jni.Local;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -41,12 +45,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Jordan");
+        owner1.setAddress("123 Main St.");
+        owner1.setCity("Monrovia");
+        owner1.setTelephone("6263595856");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Milo");
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Gertrude");
         owner2.setLastName("Stein");
+        owner2.setAddress("123. Lemon Ave.");
+        owner2.setCity("Santa Cruz");
+        owner2.setTelephone("5555876309");
+
+        Pet gersCat = new Pet();
+        gersCat.setPetType(savedDogPetType);
+        gersCat.setName("Trudy");
+        gersCat.setOwner(owner2);
+        gersCat.setBirthDate(LocalDate.now());
+        owner2.getPets().add(gersCat);
 
         ownerService.save(owner2);
 
