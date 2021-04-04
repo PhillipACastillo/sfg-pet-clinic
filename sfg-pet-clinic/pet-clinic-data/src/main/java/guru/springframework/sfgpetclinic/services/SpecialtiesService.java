d7ca47a0bd738901ca7f9fa54fa7fecaf54451ab
Phillip Castillo <PhillipACastillo@gmail.com>
2021-04-03 19:34:55 -0700

    Implement the specialtiesService and specialtyMapService for the vet object

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+import guru.springframework.sfgpetclinic.services.CrudService;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
