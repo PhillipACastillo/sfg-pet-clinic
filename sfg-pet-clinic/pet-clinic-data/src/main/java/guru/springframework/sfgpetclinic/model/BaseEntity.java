2807483a4e3417aeeb69451c0ec1ecef725e9ebe
Phillip Castillo <PhillipACastillo@gmail.com>
2021-04-04 21:18:14 -0700

    Add base entity for JPA mapping

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+// Mapped Super Class informs Spring Context that this class
+  // is only used for inheritance and therefore it does not
+  // require special attention
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
     // Use box types in case we have null values
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
