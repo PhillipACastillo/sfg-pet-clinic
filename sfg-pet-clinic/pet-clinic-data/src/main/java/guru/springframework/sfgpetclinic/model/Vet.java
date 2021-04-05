7f3c3b15973fd9e4b5481fc8b3b3c23acc3d9fbb
Phillip Castillo <PhillipACastillo@gmail.com>
2021-04-05 09:09:32 -0700

    Complete JPA mapping for Vet and Specialty

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,9 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
+    // Reminder: the enum FetchType.EAGER informs JPA
+      // To load relational data before its queried
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties",
+        joinColumns = @JoinColumn(name = "vet_id"),
+        inverseJoinColumns = @JoinColumn(name="specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
